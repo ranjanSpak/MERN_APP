f866a5469510b3a99c612ee6b3536089e216a36f
purposeeducation <70030524+purposeeducation@users.noreply.github.com>
2020-08-21 23:10:53 +0530

    Add files via upload

---
+++ client/build/precache-manifest.42c00bedea0c26bd036dd86775759b2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3127b7189d343f910a8b4f161322ed3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33dd2759457174299af5",
+    "url": "/static/js/2.feebdbdc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.feebdbdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9eaec1cc2c8a7d0f8ec7",
+    "url": "/static/js/main.e6f02d02.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
