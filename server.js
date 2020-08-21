eb6a33e4ce2e051375af696be01306ae71036c5d
purposeeducation <70030524+purposeeducation@users.noreply.github.com>
2020-08-21 22:58:22 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const morgan = require("morgan");
+const routes = require("./routes/api");
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+//Mongoose
+mongoose.connect(
+  process.env.MONGODB_URL || "mongodb://localhost:27017/mernApp",
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  }
+);
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose connection successfully setup !");
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//Tells us HTTP request in console
+app.use(morgan("tiny"));
+app.use("/", routes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.listen(PORT);
